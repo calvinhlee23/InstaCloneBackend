97264f76d5ecc7cfb5064a5abfe650038eb095be
calvinhlee23@gmail.com <calvinhlee23@gmail.com>
2020-11-16 16:18:27 -0800

    mutation setup

--- root/links/graphql_links_create_mutation.py
+++ root/links/graphql_links_create_mutation.py
@@ -1,22 +1,24 @@
 from .models import Link
 from graphene_django import DjangoObjectType
+from users.schema import UserType
 import graphene
 
 class CreateLink(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
     description = graphene.String()
+    posted_by = graphene.Field(UserType)
 
     class Arguments:
         url = graphene.String()
         description = graphene.String()
 
-    def mutate(self, info, url, description):
-        link = Link(url=url, description=description)
+    def mutate(self, info, description, url, user):
+        link = Link(description=description, posted_by=user, url=url)
         link.save()
-
         return CreateLink(
             id=link.id,
             url=link.url,
             description=link.description,
+            posted_by=graphene.Field(UserType)
         )
