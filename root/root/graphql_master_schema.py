e1b2f70642fd762da17f88efdd229449aab1cb66
calvinhlee23@gmail.com <calvinhlee23@gmail.com>
2020-11-18 21:35:29 -0800

    converted users to relay node

--- root/root/graphql_master_schema.py
+++ root/root/graphql_master_schema.py
@@ -4,10 +4,17 @@ import links.graphql_links_schema as graphql_links_schema
 import users.graphql_users_schema as graphql_users_schema
 import vote.graphql_vote_schema as graphql_vote_schema
 
-queries = [graphql_links_schema.RelayQuery,
-           graphql_users_schema.Query, graphql_vote_schema.RelayQuery]
-mutations = [graphql_links_schema.Mutation,
-             graphql_users_schema.Mutation, graphql_vote_schema.Mutation]
+queries = [
+    graphql_links_schema.RelayQuery,
+    graphql_users_schema.RelayQuery, 
+    graphql_vote_schema.RelayQuery,
+]
+
+mutations = [
+    graphql_links_schema.Mutation,
+    graphql_users_schema.Mutation, 
+    graphql_vote_schema.Mutation
+]
 
 class Query(*queries, graphene.ObjectType):
     pass
--- root/users/graphql_users_schema.py
+++ root/users/graphql_users_schema.py
@@ -1,6 +1,7 @@
 from .graphql_users_types import UserType
 from django.contrib.auth import get_user_model
 from users.graphql_users_create_mutation import CreateUser
+from graphene_django.filter import DjangoFilterConnectionField
 import graphene
 
 class Query(graphene.ObjectType):
@@ -15,6 +16,11 @@ class Query(graphene.ObjectType):
         if user.is_anonymous:
                 raise Exception('No Current User Found')
         return user
+
+class RelayQuery(graphene.ObjectType): 
+    user = graphene.relay.Node.Field(UserType)
+    users = DjangoFilterConnectionField(UserType)
+
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
