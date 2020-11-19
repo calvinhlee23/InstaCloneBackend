02fb5f1b592560b049bdcd15350fe3504246b2cb
calvinhlee23@gmail.com <calvinhlee23@gmail.com>
2020-11-18 17:09:15 -0800

    added search filter to Links query

--- root/links/graphql_links_schema.py
+++ root/links/graphql_links_schema.py
@@ -1,12 +1,21 @@
 from .graphql_links_create_mutation import CreateLink
 from .graphql_links_types import LinkType
 from .models import Link
+from django.db.models import Q
+
 import graphene
 
 class Query(graphene.ObjectType):
-    links = graphene.List(LinkType)
+    links = graphene.List(LinkType, search=graphene.String())
 
-    def resolve_links(self, info, **kwargs):
+    def resolve_links(self, info, search=None, ** kwargs):
+        if search:
+            filter = (
+                Q(url__icontains=search) |
+                Q(description__icontains=search)
+            )
+            return Link.objects.filter(filter)
+            
         return Link.objects.all()
 
 class Mutation(graphene.ObjectType):
